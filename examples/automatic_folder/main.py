ad12c34cd61d32f3c115d541b9901864c45fe731
Morgante Pell <morgantep@google.com>
2019-09-20 18:16:48 -0400

    Work on Cloud Function logging clarity

--- examples/automatic_folder/main.py
+++ examples/automatic_folder/main.py
@@ -10,9 +10,10 @@ from shutil import copytree, copyfile, ignore_patterns, rmtree
 TERRAFORM_VERSION = '0.12.8'
 
 # Download URL for Terraform
+PLATFORM = 'linux'
 TERRAFORM_DOWNLOAD_URL = (
-    'https://releases.hashicorp.com/terraform/%s/terraform_%s_linux_amd64.zip'
-    % (TERRAFORM_VERSION, TERRAFORM_VERSION))
+    'https://releases.hashicorp.com/terraform/%s/terraform_%s_%s_amd64.zip'
+    % (TERRAFORM_VERSION, TERRAFORM_VERSION, PLATFORM))
 
 # Paths where Terraform should be installed
 TERRAFORM_DIR = os.path.join('/tmp', 'terraform_%s' % TERRAFORM_VERSION)
@@ -30,20 +31,22 @@ def check_call(args, cwd=None, printOut=False):
         cwd=cwd)
     stdout, stderr = proc.communicate()
     if proc.returncode != 0:
-        print(stdout)
-        print(stderr)
+        print(stdout.decode())
+        print(stderr.decode())
         raise subprocess.CalledProcessError(
             returncode=proc.returncode,
             cmd=args)
     if printOut:
-        print(stdout)
-        print(stderr)
+        print(stdout.decode())
+        print(stderr.decode())
 
 
 def install_terraform():
     """Install Terraform."""
     if os.path.exists(TERRAFORM_PATH):
         return
+
+    print(TERRAFORM_PATH)
 
     urllib.request.urlretrieve(TERRAFORM_DOWNLOAD_URL, '/tmp/terraform.zip')
 
@@ -65,10 +68,9 @@ def handler(event, context):
     check_call([TERRAFORM_PATH, 'init'],
                   cwd=PROJECT_DIR,
                   printOut=True)
-    check_call([TERRAFORM_PATH, 'plan', '-no-color', '-lock-timeout=300s', '-out', 'tfplan'],
-                  cwd=PROJECT_DIR,
-                  printOut=True)
-    check_call([TERRAFORM_PATH, 'apply', '-no-color', '-auto-approve', '-lock-timeout=300s', 'tfplan'],
+    check_call([TERRAFORM_PATH, 'apply',
+        '-target=module.service_perimeter', '-no-color',
+        '-auto-approve', '-lock-timeout=300s'],
                   cwd=PROJECT_DIR,
                   printOut=True)
     check_call([TERRAFORM_PATH, 'output', '-json'],
