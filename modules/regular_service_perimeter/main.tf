7f1b0b079a299db2fd6c2d60073572715248d70d
Alex Weber Lopes <alex.weberlopes@wabion.com>
2021-10-07 14:20:59 +0200

    removed lifecycle part

--- modules/regular_service_perimeter/main.tf
+++ modules/regular_service_perimeter/main.tf
@@ -180,10 +180,6 @@ resource "google_access_context_manager_service_perimeter" "regular_service_peri
   }
   use_explicit_dry_run_spec = local.dry_run
 
-
-  lifecycle {
-    ignore_changes = []
-  }
 }
 
 
