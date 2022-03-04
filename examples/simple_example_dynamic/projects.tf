a3bb963d5709a1959675211c2473646b28f3a14b
Morgante Pell <morgantep@google.com>
2022-03-03 19:42:07 -0500

    fmt

--- examples/simple_example_dynamic/projects.tf
+++ examples/simple_example_dynamic/projects.tf
@@ -18,28 +18,28 @@ module "project_one" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 10.0"
 
-  name                 = "vpcsc-test-one"
-  random_project_id    = true
-  org_id               = var.parent_id
-  billing_account      = var.billing_account
+  name              = "vpcsc-test-one"
+  random_project_id = true
+  org_id            = var.parent_id
+  billing_account   = var.billing_account
 }
 
 module "project_two" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 10.0"
 
-  name                 = "vpcsc-test-two"
-  random_project_id    = true
-  org_id               = var.parent_id
-  billing_account      = var.billing_account
+  name              = "vpcsc-test-two"
+  random_project_id = true
+  org_id            = var.parent_id
+  billing_account   = var.billing_account
 }
 
 module "project_three" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 10.0"
 
-  name                 = "vpcsc-test-two"
-  random_project_id    = true
-  org_id               = var.parent_id
-  billing_account      = var.billing_account
+  name              = "vpcsc-test-two"
+  random_project_id = true
+  org_id            = var.parent_id
+  billing_account   = var.billing_account
 }
