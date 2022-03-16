89273556a8f47951ba101b51a91c6413aad7786e
Leonardo Morales <leonardo.morales@gft.com>
2022-03-16 11:51:00 -0400

    fixed policies operations type and documented

--- modules/regular_service_perimeter/main.tf
+++ modules/regular_service_perimeter/main.tf
@@ -58,11 +58,11 @@ resource "google_access_context_manager_service_perimeter" "regular_service_peri
               service_name = operations.key
               dynamic "method_selectors" {
                 for_each = operations.key != "*" ? merge(
-                  { for k, v in lookup(operations.value, "methods", {}) : v => "method" },
-                { for k, v in lookup(operations.value, "permissions", {}) : v => "permission" }) : {}
+                  { for v in lookup(operations.value, "methods", []) : v => "method" },
+                { for v in lookup(operations.value, "permissions", []) : v => "permission" }) : {}
                 content {
                   method     = method_selectors.value == "method" ? method_selectors.key : null
-                  permission = method_selectors.value == "permission" ? method_selectors.key : ""
+                  permission = method_selectors.value == "permission" ? method_selectors.key : null
                 }
               }
             }
@@ -85,11 +85,11 @@ resource "google_access_context_manager_service_perimeter" "regular_service_peri
               service_name = operations.key
               dynamic "method_selectors" {
                 for_each = operations.key != "*" ? merge(
-                  { for k, v in lookup(operations.value, "methods", {}) : v => "method" },
-                { for k, v in lookup(operations.value, "permissions", {}) : v => "permission" }) : {}
+                  { for v in lookup(operations.value, "methods", []) : v => "method" },
+                { for v in lookup(operations.value, "permissions", []) : v => "permission" }) : {}
                 content {
-                  method     = method_selectors.value == "method" ? method_selectors.key : ""
-                  permission = method_selectors.value == "permission" ? method_selectors.key : ""
+                  method     = method_selectors.value == "method" ? method_selectors.key : null
+                  permission = method_selectors.value == "permission" ? method_selectors.key : null
                 }
               }
             }
@@ -135,11 +135,11 @@ resource "google_access_context_manager_service_perimeter" "regular_service_peri
                 service_name = operations.key
                 dynamic "method_selectors" {
                   for_each = operations.key != "*" ? merge(
-                    { for k, v in lookup(operations.value, "methods", {}) : v => "method" },
-                  { for k, v in lookup(operations.value, "permissions", {}) : v => "permission" }) : {}
+                    { for v in lookup(operations.value, "methods", []) : v => "method" },
+                  { for v in lookup(operations.value, "permissions", []) : v => "permission" }) : {}
                   content {
                     method     = method_selectors.value == "method" ? method_selectors.key : null
-                    permission = method_selectors.value == "permission" ? method_selectors.key : ""
+                    permission = method_selectors.value == "permission" ? method_selectors.key : null
                   }
                 }
               }
@@ -162,11 +162,11 @@ resource "google_access_context_manager_service_perimeter" "regular_service_peri
                 service_name = operations.key
                 dynamic "method_selectors" {
                   for_each = operations.key != "*" ? merge(
-                    { for k, v in lookup(operations.value, "methods", {}) : v => "method" },
-                  { for k, v in lookup(operations.value, "permissions", {}) : v => "permission" }) : {}
+                    { for v in lookup(operations.value, "methods", []) : v => "method" },
+                  { for v in lookup(operations.value, "permissions", []) : v => "permission" }) : {}
                   content {
-                    method     = method_selectors.value == "method" ? method_selectors.key : ""
-                    permission = method_selectors.value == "permission" ? method_selectors.key : ""
+                    method     = method_selectors.value == "method" ? method_selectors.key : null
+                    permission = method_selectors.value == "permission" ? method_selectors.key : null
                   }
                 }
               }
--- modules/regular_service_perimeter/variables.tf
+++ modules/regular_service_perimeter/variables.tf
@@ -79,7 +79,7 @@ variable "shared_resources" {
 
 ## Have to solve it like this don't want use optional flag because is still experimental
 variable "egress_policies" {
-  description = "A list of all [egress policies](https://cloud.google.com/vpc-service-controls/docs/ingress-egress-rules#egress-rules-reference), each list object has a `from` and `to` value that describes egress_from and egress_to."
+  description = "A list of all [egress policies](https://cloud.google.com/vpc-service-controls/docs/ingress-egress-rules#egress-rules-reference), each list object has a `from` and `to` value that describes egress_from and egress_to.\n\nExample: `[{ from={ identities=[], identity_type=\"ID_TYPE\" }, to={ resources=[], operations={ \"SRV_NAME\"={ OP_TYPE=[] }}}}]`\n\nValid Values:\n`ID_TYPE` = `null` or `IDENTITY_TYPE_UNSPECIFIED` (only allow indentities from list); `ANY_IDENTITY`; `ANY_USER_ACCOUNT`; `ANY_SERVICE_ACCOUNT`\n`SRV_NAME` = \"`*`\" (allow all services) or [Specific Services](https://cloud.google.com/vpc-service-controls/docs/supported-products#supported_products)\n`OP_TYPE` = [methods](https://cloud.google.com/vpc-service-controls/docs/supported-method-restrictions) or [permissions](https://cloud.google.com/vpc-service-controls/docs/supported-method-restrictions)"
   type = list(object({
     from = any
     to   = any
@@ -89,7 +89,7 @@ variable "egress_policies" {
 
 ## Have to solve it like this don't want use optional flag because is still experimental
 variable "ingress_policies" {
-  description = "A list of all [ingress policies](https://cloud.google.com/vpc-service-controls/docs/ingress-egress-rules#ingress-rules-reference), each list object has a `from` and `to` value that describes ingress_from and ingress_to."
+  description = "A list of all [ingress policies](https://cloud.google.com/vpc-service-controls/docs/ingress-egress-rules#ingress-rules-reference), each list object has a `from` and `to` value that describes ingress_from and ingress_to.\n\nExample: `[{ from={ sources={ resources=[], access_levels=[] }, identities=[], identity_type=\"ID_TYPE\" }, to={ resources=[], operations={ \"SRV_NAME\"={ OP_TYPE=[] }}}}]`\n\nValid Values:\n`ID_TYPE` = `null` or `IDENTITY_TYPE_UNSPECIFIED` (only allow indentities from list); `ANY_IDENTITY`; `ANY_USER_ACCOUNT`; `ANY_SERVICE_ACCOUNT`\n`SRV_NAME` = \"`*`\" (allow all services) or [Specific Services](https://cloud.google.com/vpc-service-controls/docs/supported-products#supported_products)\n`OP_TYPE` = [methods](https://cloud.google.com/vpc-service-controls/docs/supported-method-restrictions) or [permissions](https://cloud.google.com/vpc-service-controls/docs/supported-method-restrictions)"
   type = list(object({
     from = any
     to   = any
@@ -100,7 +100,7 @@ variable "ingress_policies" {
 
 ## Have to solve it like this don't want use optional flag because is still experimental
 variable "egress_policies_dry_run" {
-  description = "A list of all [egress policies](https://cloud.google.com/vpc-service-controls/docs/ingress-egress-rules#egress-rules-reference), each list object has a `from` and `to` value that describes egress_from and egress_to."
+  description = "A list of all [egress policies](https://cloud.google.com/vpc-service-controls/docs/ingress-egress-rules#egress-rules-reference), each list object has a `from` and `to` value that describes egress_from and egress_to. Use same formatting as `egress_policies`."
   type = list(object({
     from = any
     to   = any
@@ -110,7 +110,7 @@ variable "egress_policies_dry_run" {
 
 ## Have to solve it like this don't want use optional flag because is still experimental
 variable "ingress_policies_dry_run" {
-  description = "A list of all [ingress policies](https://cloud.google.com/vpc-service-controls/docs/ingress-egress-rules#ingress-rules-reference), each list object has a `from` and `to` value that describes ingress_from and ingress_to."
+  description = "A list of all [ingress policies](https://cloud.google.com/vpc-service-controls/docs/ingress-egress-rules#ingress-rules-reference), each list object has a `from` and `to` value that describes ingress_from and ingress_to. Use same formatting as `ingress_policies`."
   type = list(object({
     from = any
     to   = any
