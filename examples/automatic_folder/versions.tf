af61cb06822784e86e9aaaaee1196770395d6910
Andrew Peabody <andrewpeabody@google.com>
2022-11-28 10:11:57 -0800

    Update versions.tf

--- examples/automatic_folder/versions.tf
+++ examples/automatic_folder/versions.tf
@@ -23,7 +23,7 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = ">= 3.0, < 5.0"
     }
     null = {
       source  = "hashicorp/null"
