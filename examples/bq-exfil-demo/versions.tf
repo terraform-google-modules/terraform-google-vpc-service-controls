a0f1b470382c6d3abea7a2d3bff257f96ea56549
Andrew Peabody <andrewpeabody@google.com>
2022-11-28 10:12:11 -0800

    Update versions.tf

--- examples/bq-exfil-demo/versions.tf
+++ examples/bq-exfil-demo/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = ">= 3.0, < 5.0"
     }
     null = {
       source = "hashicorp/null"
