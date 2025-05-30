cf117edb708115da0f5cd12f3dbcb3749176ad30
Daniel Andrade <dandrade@ciandt.com>
2025-05-29 21:50:46 -0300

    add egress rule example

---
+++ examples/simple_example_with_egress_rule/main.tf
@@ -0,0 +1,127 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "access_context_manager_policy" {
+  source  = "terraform-google-modules/vpc-service-controls/google"
+  version = "~> 7.0"
+
+  parent_id   = var.parent_id
+  policy_name = var.policy_name
+}
+
+module "access_level_members" {
+  source  = "terraform-google-modules/vpc-service-controls/google//modules/access_level"
+  version = "~> 7.0"
+
+  description = "Simple Example Access Level"
+  policy      = module.access_context_manager_policy.policy_id
+  name        = var.access_level_name
+  members     = var.members
+  regions     = var.regions
+}
+
+resource "null_resource" "wait_for_members" {
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+
+  depends_on = [module.access_level_members]
+}
+
+module "regular_service_perimeter_1" {
+  source  = "terraform-google-modules/vpc-service-controls/google//modules/regular_service_perimeter"
+  version = "~> 7.0"
+
+  policy         = module.access_context_manager_policy.policy_id
+  perimeter_name = var.perimeter_name
+
+  description   = "Perimeter shielding bigquery project"
+  resources     = [var.protected_project_ids["number"]]
+  access_levels = [module.access_level_members.name]
+
+  restricted_services = ["bigquery.googleapis.com", "storage.googleapis.com"]
+
+  egress_policies = [
+    {
+      title = "Read outside buckets from project"
+      from = {
+        sources = {
+          resources = ["projects/${var.protected_project_ids["number"]}"]
+        },
+        identity_type = "ANY_SERVICE_ACCOUNT"
+      }
+      to = {
+        resources = [
+          "projects/${var.public_project_ids["number"]}"
+        ]
+        operations = {
+          "storage.googleapis.com" = {
+            methods = [
+              "google.storage.objects.get",
+              "google.storage.objects.list"
+            ]
+          }
+        }
+      }
+    },
+    {
+      title = "Use permissions for Big Query access" # See https://cloud.google.com/vpc-service-controls/docs/supported-method-restrictions
+      from = {
+        sources = {
+          resources = ["projects/${var.protected_project_ids["number"]}"]
+        },
+        identity_type = "ANY_SERVICE_ACCOUNT"
+      }
+      to = {
+        resources = [
+          "projects/${var.public_project_ids["number"]}"
+        ]
+        operations = {
+          "bigquery.googleapis.com" = {
+            permissions = [
+              "bigquery.datasets.get",
+              "bigquery.models.getData",
+              "bigquery.models.getMetadata",
+              "bigquery.models.list",
+              "bigquery.tables.get",
+              "bigquery.tables.getData",
+              "bigquery.tables.list"
+            ]
+          }
+        }
+      }
+    },
+  ]
+
+  shared_resources = {
+    all = [var.protected_project_ids["number"]]
+  }
+
+  depends_on = [
+    module.gcs_buckets
+  ]
+}
+
+module "gcs_buckets" {
+  source           = "terraform-google-modules/cloud-storage/google"
+  version          = "~> 10.0"
+  project_id       = var.public_project_ids["id"]
+  names            = var.buckets_names
+  randomize_suffix = true
+  prefix           = var.buckets_prefix
+  set_admin_roles  = true
+  admins           = var.members
+}
---
+++ examples/simple_example_with_egress_rule/variables.tf
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "parent_id" {
+  description = "The parent of this AccessPolicy in the Cloud Resource Hierarchy. As of now, only organization are accepted as parent."
+  type        = string
+}
+
+variable "policy_name" {
+  description = "The policy's name."
+  type        = string
+}
+
+variable "protected_project_ids" {
+  description = "Project id and number of the project INSIDE the regular service perimeter. This map variable expects an \"id\" for the project id and \"number\" key for the project number."
+  type        = object({ id = string, number = number })
+}
+
+variable "public_project_ids" {
+  description = "Project id and number of the project OUTSIDE the regular service perimeter. This map variable expects an \"id\" for the project id and \"number\" key for the project number."
+  type        = object({ id = string, number = number })
+}
+
+variable "members" {
+  description = "An allowed list of members (users, service accounts). The signed-in identity originating the request must be a part of one of the provided members. If not specified, a request may come from any user (logged in/not logged in, etc.). Formats: user:{emailid}, serviceAccount:{emailid}"
+  type        = list(string)
+}
+
+variable "regions" {
+  description = "The request must originate from one of the provided countries/regions. Format: A valid ISO 3166-1 alpha-2 code."
+  type        = list(string)
+  default     = []
+}
+
+variable "perimeter_name" {
+  description = "Perimeter name of the Access Policy.."
+  type        = string
+  default     = "regular_perimeter_e"
+}
+
+variable "access_level_name" {
+  description = "Access level name of the Access Policy."
+  type        = string
+  default     = "terraform_members_e"
+}
+
+variable "read_bucket_identities" {
+  description = "List of all identities should get read access on bucket"
+  type        = list(string)
+  default     = []
+}
+
+variable "buckets_prefix" {
+  description = "Bucket Prefix"
+  type        = string
+  default     = "test-bucket-e"
+}
+
+variable "buckets_names" {
+  description = "Buckets Names as list of strings"
+  type        = list(string)
+  default     = ["bucket1-e", "bucket2-e"]
+}
