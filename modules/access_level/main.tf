c6ba03e48c1b09ef011d4b36af0f6b1636cfedbe
Maria Martins <mariamartins@ciandt.com>
2026-06-26 09:39:33 -0300

    add lifecycle create before destroy

--- modules/access_level/main.tf
+++ modules/access_level/main.tf
@@ -62,4 +62,7 @@ resource "google_access_context_manager_access_level" "access_level" {
 
     combining_function = var.combining_function
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
